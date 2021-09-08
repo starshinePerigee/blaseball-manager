2777e650748e4c4eb50b608d1c43abca43cd228d
starshinePerigee <skywing.ki@gmail.com>
2021-09-08 04:54:50 -0700

    Added better display in descriptors.py; fixed some bugs

--- blaseball/util/descriptors.py
+++ blaseball/util/descriptors.py
@@ -17,17 +17,17 @@ from data.playerdescriptors import ASPECTS, THRESHOLDS, DESCRIPTOR_DB
 from blaseball.util.weighter import calculate_weighted
 
 
-ASPECT_THRESHOLD = 0.75
+ASPECT_THRESHOLD = 0.8
 
 
 def get_descriptor(player, stat) -> str:
     aspect_weights = {aspect: calculate_weighted(player, ("descriptor_" + aspect)) for aspect in ASPECTS[stat]}
     aspects_sorted = sorted((v, k) for k, v in aspect_weights.items())
     aspects_sorted.reverse()
-    high_threshold = aspects_sorted[0][0]
+    high_threshold = aspects_sorted[0][0] * ASPECT_THRESHOLD
     total_above_threshold = sum([(1 if x[0] > high_threshold else 0) for x in aspects_sorted])
     if total_above_threshold == 1:
-        primary = aspects_sorted[0][1],
+        primary = aspects_sorted[0][1]
         secondary = primary
     elif total_above_threshold == len(aspect_weights):
         primary = 'All'
@@ -51,5 +51,19 @@ if __name__ == "__main__":
     from blaseball.stats import players
     pb = players.PlayerBase(3)
     for i in range(1001, 1004):
-        print(pb[i].text_breakdown())
-        print("\r\n\r\n*******************\r\n\r\n")
+        text = pb[i].text_breakdown().split("\r\n")
+        lines = [4, 5, 10, 11, 12, 13, 14, 15] + list(range(53, 94))
+        for j in lines:
+            #print(f"{j}: {text[j]}")
+            print(text[j])
+
+        for stat in ['batting']:
+            p_weights = {aspect: calculate_weighted(pb[i], ("descriptor_" + aspect)) for aspect in ASPECTS[stat]}
+            p_sorted = sorted((v, k) for k, v in p_weights.items())
+            p_sorted.reverse()
+            for weight in p_sorted:
+                print(f"{weight[1]}: {weight[0]:.3f}")
+            thresh = p_sorted[0][0] * ASPECT_THRESHOLD
+            print(f"threshold: {thresh:.3f}")
+
+        print("\r\n\r\n--------------------------\r\n\r\n")
